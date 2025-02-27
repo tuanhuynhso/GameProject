a589a599416f2da22963ff500b1b7fa8ea4060d3
OshiroDrey <minhtoan2746@gmail.com>
2025-02-27 14:48:51 +0700

    improve render efficiency
    only draw tile when it is in player's camera

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -94,7 +94,17 @@ public class TileManager {
             int worldY=worldRow*gp.tileSize;
             int screenX= worldX - gp.player.worldX + gp.player.screenX;
             int screenY= worldY - gp.player.worldY + gp.player.screenY;
-            g2d.drawImage(tile[mapTileNum[worldCol][worldRow]].image,screenX,screenY,gp.tileSize, gp.tileSize, null);
+
+            if(
+            worldX+gp.tileSize>gp.player.worldX-gp.player.screenX&&
+            worldX-gp.tileSize<gp.player.worldX+gp.player.screenX&&
+            worldY+gp.tileSize>gp.player.worldY-gp.player.screenY&&
+            worldY-gp.tileSize<gp.player.worldY+gp.player.screenY){
+
+
+                g2d.drawImage(tile[mapTileNum[worldCol][worldRow]].image,screenX,screenY,gp.tileSize, gp.tileSize, null);
+            }
+
             worldCol++;
 
 

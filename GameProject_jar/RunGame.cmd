5408f6e3eb5af53d89c32ab041958fcce4012aca
TriRvn <trinhminhtri317@gmail.com>
2025-06-12 22:37:01 +0700

    Update RunGame.cmd

--- GameProject_jar/RunGame.cmd
+++ GameProject_jar/RunGame.cmd
@@ -3,6 +3,6 @@ title Katavania Launcher
 REM Change directory to the script's own directory
 cd /c "C:\Users\WIN10\Downloads\GameProject\out\artifacts\GameProject_jar"
 echo Launching Katavania...
-java -jar GamProject.jar
+java -jar GameProject.jar
 REM You can add a pause below if you want the window to stay open after the game closes or if there's an error
 REM pause

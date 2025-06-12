1eb824128157a8a8e858e3b266dda743783fcac5
TriRvn <trinhminhtri317@gmail.com>
2025-06-12 22:31:19 +0700

    Add files via upload

---
+++ GameProject_jar/RunGame.cmd
@@ -0,0 +1,8 @@
+@echo off
+title Katavania Launcher
+REM Change directory to the script's own directory
+cd /c "C:\Users\WIN10\Downloads\GameProject\out\artifacts\GameProject_jar"
+echo Launching Katavania...
+java -jar GamProject.jar
+REM You can add a pause below if you want the window to stay open after the game closes or if there's an error
+REM pause
---
+++ GameProject_jar/RunGame.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Get the directory where the script is located
+DIR="C:\Users\WIN10\Downloads\GameProject\out\artifacts\GameProject_jar"
+# Change to that directory
+cd "$DIR"
+echo "Launching Katavania..."
+java -jar GameProject.jar
